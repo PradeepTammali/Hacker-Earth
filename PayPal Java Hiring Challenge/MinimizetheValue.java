5deb26bb63e9d65e3fd56332352cf70477587a6a
tcs <pradeepkumartammali@gmail.com>
2019-02-21 18:48:41 +0530

    Adding MinimizetheValue file

--- PayPal Java Hiring Challenge/MinimizetheValue.java
+++ PayPal Java Hiring Challenge/MinimizetheValue.java
@@ -3,6 +3,9 @@
    node and key value*/
 
 import java.util.Scanner;
+
+import com.sun.glass.ui.Robot;
+
 import java.util.Arrays;
 
 class Node {
@@ -39,8 +42,52 @@ class MinimizetheValue {
         }
     }
 
-    public void levelOrderInsert(Node root, int key, int value) {
-        int h = heightOfTree(root);
+    public int levelOrderWeight(int h, Node root, int key) {
+        int level = 0;
+        int levelWeight = 0;
+        for (int i = 1; i <= h; i++) {
+            if (findLevel(h, root, key, i)) {
+                level = i;
+                break;
+            }
+        }
+        if (level > 0) {
+            levelWeight = findlevelWeight(root, level, key);
+        }
+        return levelWeight;
+    }
+
+    public boolean findLevel(int height, Node root, int key, int level) {
+        boolean isFound = false;
+        if (root == null)
+            return false;
+        if (level == 1) {
+            if (root.key == key) {
+                isFound = true;
+            }
+        } else if (level > 1) {
+            isFound = findLevel(height, root.left, key, level - 1);
+            if (!isFound) {
+                isFound = findLevel(height, root.right, key, level - 1);
+            }
+        }
+        return isFound;
+    }
+
+    public int findlevelWeight(Node root, int level, int key) {
+        int weightOfLevel = 0;
+        if (root == null)
+            return 0;
+        if (level == 1) {
+            return root.value;
+        } else if (level > 1) {
+            weightOfLevel += findlevelWeight(root.left, level - 1, key);
+            weightOfLevel += findlevelWeight(root.right, level - 1, key);
+        }
+        return weightOfLevel;
+    }
+
+    public void levelOrderInsert(int h, Node root, int key, int value) {
         int i;
         for (i = 1; i <= h; i++)
             if (addNewNode(root, i, key, value)) {
@@ -52,14 +99,14 @@ class MinimizetheValue {
     public boolean addNewNode(Node root, int level, int newKey, int newValue) {
         boolean isInserted = false;
         if (root == null)
-            return isInserted;
+            return false;
         if (level == 1) {
             if (root.left == null) {
                 root.left = new Node(newKey, newValue);
                 isInserted = true;
             } else if (root.right == null) {
                 root.right = new Node(newKey, newValue);
-                isInserted = false;
+                isInserted = true;
             }
         } else if (level > 1) {
             isInserted = addNewNode(root.left, level - 1, newKey, newValue);
@@ -72,19 +119,31 @@ class MinimizetheValue {
 
     public boolean searchInsert(Node node, int u, int v, int[] values) {
         boolean isInserted = false;
+        if (node == null)
+            return false;
         if (node.key == u) {
             if (node.left == null) {
                 node.left = new Node(v, values[v - 1]);
                 isInserted = true;
-                return isInserted;
             } else {
                 node.right = new Node(v, values[v - 1]);
                 isInserted = true;
-                return isInserted;
+            }
+        } else if (node.key == v) {
+            if (node.left == null) {
+                node.left = new Node(u, values[u - 1]);
+                isInserted = true;
+            } else {
+                node.right = new Node(u, values[u - 1]);
+                isInserted = true;
             }
         } else {
-            return searchInsert(node.left, u, v, values) || searchInsert(node.right, u, v, values);
+            isInserted = searchInsert(node.left, u, v, values);
+            if (!isInserted) {
+                isInserted = searchInsert(node.right, u, v, values);
+            }
         }
+        return isInserted;
     }
 
     public static void main(String[] args) {
@@ -99,13 +158,26 @@ class MinimizetheValue {
             int u = Integer.parseInt(u_v[0]);
             int v = Integer.parseInt(u_v[1]);
             if (tree.root == null) {
-                tree.root = new Node(u, values[u - 1]);
-                tree.root.left = new Node(v, values[v - 1]);
+                if (u < v) {
+                    tree.root = new Node(u, values[u - 1]);
+                    tree.root.left = new Node(v, values[v - 1]);
+                } else {
+                    tree.root = new Node(v, values[v - 1]);
+                    tree.root.left = new Node(u, values[u - 1]);
+                }
             } else {
                 tree.searchInsert(tree.root, u, v, values);
             }
         }
-        tree.levelOrderInsert(tree.root, N + 1, X);
+        int intialWeight = X;
+        for (int i : values) {
+            intialWeight += i;
+        }
+        int h = tree.heightOfTree(tree.root);
+        tree.levelOrderInsert(h, tree.root, N + 1, X);
+        int branchWeight = tree.levelOrderWeight(h, tree.root, N + 1);
+        int finalWeight = branchWeight + intialWeight;
+        System.out.println(finalWeight);
         s.close();
     }
 
